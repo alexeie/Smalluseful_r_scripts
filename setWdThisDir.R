525c7b245c8925229e97a8b11b5329f4d0f09365
Alexander K. Eieland <alexeieland@gmail.com>
2019-09-20 16:49:38 +0200

    Delete setWdThisDir.R

--- setWdThisDir.R
+++
@@ -1,8 +0,0 @@
-# This function sets working directory to the directory 
-# where the script executing the file is located
-
-setWdThisDir = function () {
-    current_path <- rstudioapi::getActiveDocumentContext()$path
-    setwd(dirname(current_path))
-    rm(current_path)
-}
